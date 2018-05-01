2b130823cfb16ba5d032ca96bf60167065e2fc53
zhaozichuan <zhaozichuan@gmail.com>
2018-05-01 18:32:07 +0800

    wx2

---
+++ client/pages/mark/mark.js
@@ -0,0 +1,35 @@
+Page({
+  data:{
+    // text:"这是一个页面"
+    res:{}
+  },
+  onLoad:function(options){
+   var that=this;
+    // 页面初始化 options为页面跳转所带来的参数
+   wx.getStorage({
+     key: 'res',
+     success: function (res) {
+       console.log('---.'+res.data)
+
+       that.setData({
+         res: res.data
+       });
+     }
+   })
+   
+   
+      
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
